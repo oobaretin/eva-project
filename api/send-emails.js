d4f836f8118a2faa2887f9c0e49c8e6f7f1a611a
Osagie Obaretin <osagieobaretin@awas-MacBook-Pro.local>
2025-09-28 15:07:32 -0500

    Fix Vercel API endpoint syntax for email service

--- api/send-emails.js
+++ api/send-emails.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransporter({
   },
 });
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
